d725ce1598e66ee346b3cfeff9936c9a09904c97
Admin <preciouspeter3173@gmail.com>
2023-06-03 11:35:17 +0100

    First screen created

--- App.js
+++ App.js
@@ -1,10 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import React from 'react';
 
+
+
+  
 export default function App() {
+
+  const[text, onChangeText] = React.useState('Enter Text');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+
+        <Image
+        
+        source = {require('./assets/note.png')}
+        style= {styles.imageArrange}
+        />
+        <Text style= {styles.textColor}>
+          My NotePad
+          
+        </Text>
+
+        
+        <TextInput
+        style = {styles.inputTextDesign}
+        editable
+        multiline
+        numberOfLines={1}
+        onChangeText={text => onChangeText(text
+          )}
+        value={text}
+
+
+        
+        />
+      </View>
+      <View>
+
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +48,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+   
   },
+  imageArrange:{
+
+    width: 50,
+    height:50,
+    padding:50
+
+  },
+
+  textColor:{
+    fontWeight: 'bold',
+    color: 'white',
+    fontSize: 18,
+    marginTop: 2
+  },
+
+  inputTextDesign:{
+    backgroundColor: "white",
+    padding : 2,
+    width :100,
+    marginTop: 50
+  }
 });
